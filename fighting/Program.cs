6942da47a2793bf3eafcd4bb48358708bde9c542
Abdalhadi <abdalhadi.darkoushe@elev.ga.ntig.se>
2024-09-18 21:13:46 +0200

    hh

--- fighting/Program.cs
+++ fighting/Program.cs
@@ -1,1 +1,35 @@
 ﻿
+while (true)
+{
+    int hp = Random.Shared.Next();
+Console.WriteLine(hp);
+Console.ReadLine();
+}
+
+
+Console.WriteLine("Hej och välkommen till NTI skolan för att gå vidare så behöver du skriva upp ditt namn");
+Console.WriteLine("skriv upp ditt nam här:");
+
+string name = Console.ReadLine();
+
+
+
+
+  while (true)
+{
+    while
+} (true)
+{
+    while
+} (name == "")
+{
+    Console.WriteLine("tyvär så måste du skriva upp ditt namn");
+
+    Console.WriteLine("försök igen!!");
+    name = Console.ReadLine();
+}
+
+
+
+    
+Console.ReadLine();
